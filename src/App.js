864c5008c0594a5ba727ee5497023fc77436fd53
Devarat Patel <devaratpatel1@gmail.com>
2021-10-09 18:44:21 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Folder name="Desktop">
+        <Folder name="Music">
+          <File name="all_star.mp3." />
+          <File name="express_file.mp3" />
+        </Folder>
+        <Folder name="Random">
+          <File name="weirdText.txt" />
+        </Folder>
+        <File name="dogs.jpeg" />
+        <File name="cats.png" />
+      </Folder>
+      <Folder name="Applications">
+        <Folder name="Facetime" />
+        <Folder name="Adobe Acrobat Reader">
+          <File name="covidTest.pdf" />
+          <File name="passport.pdf" />
+        </Folder>
+      </Folder>
     </div>
   );
-}
+};
 
+const Folder = (props) => {
+  const [isOpen, setIsOpenFunc] = useState(true);
+  const { name, children } = props;
+
+  const direction = isOpen ? "down" : "right";
+  const handleClick = () => {
+    setIsOpenFunc(!isOpen);
+  };
+
+  return (
+    <div>
+      <span onClick={handleClick}>
+        <i className="blue folder icon" />
+        <i className={`caret ${direction} icon`} />
+      </span>
+      {name}
+      <div style={{ marginLeft: "18px" }}>{isOpen ? children : null}</div>
+    </div>
+  );
+};
+
+const File = (props) => {
+  const { name } = props;
+  const fileExtension = name.split(".")[1];
+  const fileIcons = {
+    mp3: "headphones",
+    txt: "file outline icon",
+    jpeg: "file image",
+    png: "file image outline",
+    pdf: "file outline pdf",
+  };
+  return (
+    <div>
+      {name}
+      <i className={`${fileIcons[fileExtension]} icon`} />
+    </div>
+  );
+};
 export default App;
